263f79933131aefb8e8221043c075648c173855c
JorshuaG <joshbgearheart@gmail.com>
2021-12-04 16:54:17 -1000

    initial creation, navbar, headerconent, profilecontent

---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <div className="App-header">
+      <div>
+        <h2>Joshua Gearheart</h2>
+      </div>
+      <div>
+         <div class="button" id="button-6">
+            <div id="spin"></div>
+            <a href="#">About Me</a>
+        </div>
+          <div class="button" id="button-6">
+    <div id="spin"></div>
+    <a href="#">Projects</a>
+  </div>
+          <div class="button" id="button-6">
+    <div id="spin"></div>
+    <a href="#">SignIn</a>
+  </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/profileContent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ProfileContent(props) {
+  return (
+    <section className="profile">
+      <div className="profileContent">
+        <img
+          src={`${process.env.PUBLIC_URL}/josh.JPG`}
+          alt="Josh Gearheart wearing a gray shirt"
+        ></img>
+        <div className="profileText">
+          “He felt that his whole life was some kind of dream and he sometimes wondered whose it was and whether they were enjoying it.”
+― Douglas Adams, The Hitchhiker's Guide to the Galaxy
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProfileContent;

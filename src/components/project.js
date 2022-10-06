7adf9cb220fc4e2941dcee7a1ee51319527f4b0b
JorshuaG <joshbgearheart@gmail.com>
2022-10-06 11:33:34 -1000

    added social icons, linked to social pages, updated prooject link icons

--- src/components/project.js
+++ src/components/project.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 function Project(props) {
   return (
     <div className="ProjectItem">
       <h3>{props.projectItem.projectTitle}</h3>
       <p>Description of the project</p>
-      <img
-        className="projectLinkImg"
-        src={`${process.env.PUBLIC_URL}/linkTransparent.gif`}
-        alt="A placeholder image url"
-      />
+      <div className="projectLinks">
+        <a>
+          <Icon icon="akar-icons:github-fill" color="white" width="2rem" />
+        </a>
+        <a>
+          <Icon icon="akar-icons:link-on" color="white" width="2rem" />
+        </a>
+      </div>
     </div>
   );
 }
